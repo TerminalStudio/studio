fb3c110af1c53a8d88629f28374bdcf9f57d3d70
xuty <xty50337@hotmail.com>
2020-08-09 21:57:32 +0800

    update

---
+++ studio/README.md
@@ -0,0 +1,7 @@
+## TerminalStudio
+
+Terminal emulator written in **Flutter**.
+
+*Comming soon.*
+
+<img width="400px" src="https://raw.githubusercontent.com/TerminalStudio/studio/master/media/demo.png">
--- studio/linux/my_application.cc
+++ studio/linux/my_application.cc
@@ -4,27 +4,35 @@
 
 #include "flutter/generated_plugin_registrant.h"
 
-struct _MyApplication {
+struct _MyApplication
+{
   GtkApplication parent_instance;
 };
 
 G_DEFINE_TYPE(MyApplication, my_application, GTK_TYPE_APPLICATION)
 
 // Implements GApplication::activate.
-static void my_application_activate(GApplication* application) {
-  GtkWindow* window =
+static void my_application_activate(GApplication *application)
+{
+  g_object_set(gtk_settings_get_default(),
+               "gtk-application-prefer-dark-theme", TRUE,
+               NULL);
+
+  GtkWindow *window =
       GTK_WINDOW(gtk_application_window_new(GTK_APPLICATION(application)));
-  GtkHeaderBar *header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
-  gtk_widget_show(GTK_WIDGET(header_bar));
-  gtk_header_bar_set_title(header_bar, "studio");
-  gtk_header_bar_set_show_close_button(header_bar, TRUE);
-  gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
+  // GtkHeaderBar *header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
+  // gtk_widget_show(GTK_WIDGET(header_bar));
+  // gtk_header_bar_set_title(header_bar, "studio");
+  // gtk_header_bar_set_show_close_button(header_bar, TRUE);
+  // gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
+  gtk_window_set_title(window, "Terminal Studio");
   gtk_window_set_default_size(window, 1280, 720);
   gtk_widget_show(GTK_WIDGET(window));
+  // gtk_widget_set_opacity (GTK_WIDGET(window), 0.5);
 
   g_autoptr(FlDartProject) project = fl_dart_project_new();
 
-  FlView* view = fl_view_new(project);
+  FlView *view = fl_view_new(project);
   gtk_widget_show(GTK_WIDGET(view));
   gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(view));
 
@@ -33,13 +41,15 @@ static void my_application_activate(GApplication* application) {
   gtk_widget_grab_focus(GTK_WIDGET(view));
 }
 
-static void my_application_class_init(MyApplicationClass* klass) {
+static void my_application_class_init(MyApplicationClass *klass)
+{
   G_APPLICATION_CLASS(klass)->activate = my_application_activate;
 }
 
-static void my_application_init(MyApplication* self) {}
+static void my_application_init(MyApplication *self) {}
 
-MyApplication* my_application_new() {
+MyApplication *my_application_new()
+{
   return MY_APPLICATION(g_object_new(my_application_get_type(),
                                      "application-id", APPLICATION_ID,
                                      nullptr));
