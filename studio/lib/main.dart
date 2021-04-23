f9bc486dc4e3215086e6e7c29b4571aefe5982bd
devmil <info@devmil.de>
2021-04-23 18:58:00 +0200

    Switch to BlockingPseudoTerminal

--- studio/lib/main.dart
+++ studio/lib/main.dart
@@ -102,7 +102,7 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
       shell,
       // ['-l'],
       [],
-      blocking: false,
+      blocking: true,
     );
 
     final backend = PtyTerminalBackend(pty);
